20ac4b931b8c4ee14ca9c7c1b5e9eec06b9a2a0e
Gabriel Castro <gabrielcastro.ec@gmail.com>
2025-11-11 21:34:40 -0300

    Added RG example in doc_extractor, fix multi-agent architecture

--- src/agents/doc_data_extractor/agent.py
+++ src/agents/doc_data_extractor/agent.py
@@ -1,5 +1,5 @@
 # from .pydantic_schema import OutputSchema
-from .pydantic_schema import CNHdata
+from .pydantic_schema import CNHdata, RGdata
 from google.adk.agents import LlmAgent, SequentialAgent
 import textwrap
 
@@ -47,19 +47,41 @@ cnh_agent = LlmAgent(
     description="Extrai os dados de uma CNH",
     instruction=cnh_instruction,
     output_schema=CNHdata,
-    output_key="document_data"
+    output_key="document_data",
+    disallow_transfer_to_parent=True, 
+    disallow_transfer_to_peers=True
 )
 
-# Cria pipeline sequencial (extração dos dados -> exibição dos dados)
-cnh_pipeline = SequentialAgent(name="cnh_pipeline", sub_agents=[cnh_agent, user_view_agent])
+"""Agente responsável por extrair dados de um RG"""
+# Instrução do Agente
+rg_instruction = textwrap.dedent("""\
+    Você é responsável por extrair os dados de uma Carteira de Identidade (RG)
+    
+    Dadas as imagens do documento, extraia:
+    - tipo_do_documento: Tipo do documento recebido (Carteira Nacional de habilitação - CNH, Registro Geral - RG, ...)
+    - nome_completo: Nome completo da pessoa física
+    - cpf: CPF da pessoa física presente no documento
+    - data_de_nascimento: Data de nascimento da pessoa física presente no documento
 
-# --- Insira aqui mais 2 agentes ---
+    Responda seguindo o formato JSON especificado, usando como base apenas as imagens dadas""")
+
+# Definição do Agente
+rg_agent = LlmAgent(
+    name="carteira_de_identidade_rg",
+    model="gemini-2.5-flash",
+    description="Extrai os dados de uma Carteira de Identidade (RG)",
+    instruction=rg_instruction,
+    output_schema=RGdata,
+    output_key="document_data",
+    disallow_transfer_to_parent=True, 
+    disallow_transfer_to_peers=True
+)
+
+# --- Insira aqui agentes extratores de dados adicionais ---
 
 # Instrução do Agente
 
 # Definição do Agente
-
-# Pipeline Sequencial (extração dos dados -> exibição dos dados)
 
 # ---
 
@@ -69,7 +91,10 @@ coordinator_instruction = textwrap.dedent("""\
     Você é especialista em coordenar Agentes responsáveis por extrair dados de um documento com foto.
 
     Dada a imagem do documento, escolha o agente que irá extrair os dados, conforme a seguinte descrição:
-    - carteira_nacional_de_habilitacao_cnh: Extrai os dados de uma Carteira Nacional de Habilitação (CNH)
+    - 'carteira_nacional_de_habilitacao_cnh': Extrai os dados de uma Carteira Nacional de Habilitação (CNH)
+    - 'carteira_de_identidade_rg': Extrai os dados de uma Carteira de Identidade (RG)
+    
+    Após extração dos dados do documento use o agente 'user_view_agent' para exibir o resultado ao usuário.
     """)
 
 # Definição do agente
@@ -79,7 +104,9 @@ coordinator = LlmAgent(
     description="Coordena agentes extratores de dados de documentos",
     instruction=coordinator_instruction,
     sub_agents=[ 
-        cnh_pipeline,
+        cnh_agent,
+        rg_agent,
+        user_view_agent
         # Adicione os agentes aqui
     ]
 )
--- src/agents/doc_data_extractor/pydantic_schema.py
+++ src/agents/doc_data_extractor/pydantic_schema.py
@@ -1,7 +1,15 @@
 from pydantic import BaseModel, Field
+from typing import List
 
 class CNHdata(BaseModel):
     tipo_do_documento: str = Field(description="Tipo do documento (CNH, RG, ...)")
     nome_completo: str = Field(description="Nome Completo presente no documento")
     cpf: str = Field(description="CPF da pessoa física presente no documento")
     data_de_nascimento: str = Field(description="Data de nascimento da pessoa física presente no documento")
+
+class RGdata(BaseModel):
+    tipo_do_documento: str = Field(description="Tipo do documento (CNH, RG, ...)")
+    nome_completo: str = Field(description="Nome Completo presente no documento")
+    cpf: str = Field(description="CPF da pessoa física presente no documento")
+    data_de_nascimento: str = Field(description="Data de nascimento da pessoa física presente no documento")
+    filiacao: List[str] = Field(description="Filiação da pessoa física presente no documento")
